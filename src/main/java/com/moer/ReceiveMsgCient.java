efaf2881f3a72594d3371ce56d8fb507cdf51c90
gaoxuejian <gaoxuejian@moer.cn>
2018-09-03 18:28:58 +0800

    完善测试的客户端 下一步测试  多用户 多结点的异常错误情况

--- src/main/java/com/moer/ReceiveMsgCient.java
+++ src/main/java/com/moer/ReceiveMsgCient.java
@@ -1,6 +1,7 @@
 package com.moer;
 
 import com.alibaba.fastjson.JSON;
+import com.moer.common.Constant;
 import com.moer.entity.ImSession;
 import com.moer.util.HttpUtil;
 
@@ -24,42 +25,69 @@ class ImClient extends Thread
     public static final String PULL_URL = "http://%s/pull?uid=%s&sessionid=%s";
     @Override
     public void run() {
-        Map<String,String> cookies = new HashMap<>();
-        cookies.put("_jm_ppt_id","WPLNkDBlepHjltBs8qQDHThpqTnKzxhzU_XEhiW31CEUdrQCjBOQZEkhAqXpaDQYUk-uVw8k3x4zInSpacrFiA1389_7nLSTOqAhB2IKQ-bvlK3MLGKSEKMW");
-        String result = HttpUtil.doGet(String.format(INIT_URL,"100809070"),null, cookies, null,0,0,0);
-        Map<String, Object> resultMap = JSON.parseObject(result, Map.class);
-        int code = Integer.valueOf(resultMap.get("code").toString());
-        if (code != 1000) {
-            System.out.println("call " + INIT_URL + " failed with message :" + resultMap.get("message"));
-            return;
-        }
-        Map<String,String> data = (Map<String, String>) resultMap.get("data");
-        String serverAddr = data.get("addr");
-        String token = data.get("token");
-        String uid = data.get("uid");
-        result = HttpUtil.doGet(String.format(CONNECT_URL, serverAddr,uid, token, ImSession.SESSION_SOURCE_WEB));
-        resultMap = JSON.parseObject(result, Map.class);
-        if (resultMap == null) {
-            System.out.println("call " + CONNECT_URL + " exception with message :" + result);
-            return;
-        }
-        code = Integer.valueOf(resultMap.get("code").toString());
-        if (code != 1000) {
-            System.out.println("call " + CONNECT_URL + " failed with message :" + resultMap.get("message"));
-            return;
-        }
-        data = (Map<String, String>) resultMap.get("data");
-        String sessionId = data.get("sessionId");
-        while(true) {
-            try {
-                result = HttpUtil.doGet(String.format(PULL_URL, serverAddr, uid, sessionId),null,null,null, 90000, 90000, 90000);
-                resultMap = JSON.parseObject(result, Map.class);
-                System.out.println(JSON.toJSONString(resultMap));
-                Thread.sleep(1000);
-            }catch (Exception e){
-                System.out.println(e.getMessage());
+        while (true) {
+            Map<String, String> cookies = new HashMap<>();
+            cookies.put("_jm_ppt_id", "WPLNkDBlepHjltBs8qQDHThpqTnKzxhzU_XEhiW31CEUdrQCjBOQZEkhAqXpaDQYUk-uVw8k3x4zInSpacrFiA1389_7nLSTOqAhB2IKQ-bvlK3MLGKSEKMW");
+            String result = HttpUtil.doGet(String.format(INIT_URL, "100809070"), null, cookies, null, 0, 0, 0);
+            Map<String, Object> resultMap = JSON.parseObject(result, Map.class);
+            int code = Integer.valueOf(resultMap.get("code").toString());
+            //这种情况说明后端出错 应该重试  其他情况是前台错误 程序终止
+            if (code != Constant.CODE_SUCCESS ) {
+                System.out.println("call " + INIT_URL + " failed with message :" + resultMap.get("message"));
+                if (code == Constant.CODE_NO_SERVER_NODE) {
+                    try {
+                        Thread.sleep(1000);
+                    }catch (Exception e){}
+                    continue;
+                }else {
+                    return;
+                }
             }
-        }
+            Map<String, String> data = (Map<String, String>) resultMap.get("data");
+            String serverAddr = data.get("addr");
+            String token = data.get("token");
+            String uid = data.get("uid");
+            while (true) {
+                result = HttpUtil.doGet(String.format(CONNECT_URL, serverAddr, uid, token, ImSession.SESSION_SOURCE_WEB));
+                resultMap = JSON.parseObject(result, Map.class);
+                if (resultMap == null) {
+                    System.out.println("call " + CONNECT_URL + " exception with message :" + result);
+                    return;
+                }
+                code = Integer.valueOf(resultMap.get("code").toString());
+                if (code != Constant.CODE_SUCCESS) {
+                    System.out.println("call " + CONNECT_URL + " failed with message :" + resultMap.get("message"));
+                    if (code == Constant.CODE_NODE_EXPIRED) {
+                        break;
+                    }else {
+                        return;
+                    }
 
+                }else {
+                    data = (Map<String, String>) resultMap.get("data");
+                    String sessionId = data.get("sessionId");
+                    while (true) {
+                        try {
+                            result = HttpUtil.doGet(String.format(PULL_URL, serverAddr, uid, sessionId), null, null, null, 90000, 90000, 90000);
+                            resultMap = JSON.parseObject(result, Map.class);
+                            code = Integer.valueOf(resultMap.get("code").toString());
+                            if (code != Constant.CODE_SUCCESS) {
+                                System.out.println("call " + CONNECT_URL + " failed with message :" + resultMap.get("message"));
+                                if (code == Constant.CODE_UNCONNECT) {
+                                    break;
+                                }else {
+                                    return;
+                                }
+                            }
+                            System.out.println(JSON.toJSONString(resultMap));
+                            Thread.sleep(1000);
+                        } catch (Exception e) {
+                            System.out.println(e.getMessage());
+                        }
+                    }
+                }
+            }
+
+        }
     }
 }
